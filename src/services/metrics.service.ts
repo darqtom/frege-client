ba3d0733a745bfbbc485d7112c7632c15220e837
Dariusz Tomczyszyn <dariusz.tomczyszyn@swmansion.com>
2022-04-27 20:55:24 +0200

    fix(repository): remove unnecessary try-catch block in fetchRepository

--- src/services/metrics.service.ts
+++ src/services/metrics.service.ts
@@ -3,19 +3,15 @@ import axios from "axios";
 import { RepositoryResult } from "../models/RepositoryResult";
 
 export async function fetchRepository(url: string) {
-  try {
-    const { data } = await axios.get<RepositoryResult[]>(
-      "http://localhost:8080/repositories",
-      {
-        data: {
-          url: url,
-        },
-      }
-    );
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
+  const { data } = await axios.get<RepositoryResult[]>(
+    "http://localhost:8080/repositories",
+    {
+      data: {
+        url: url,
+      },
+    }
+  );
+  return data;
 }
 
 export async function fetchRepositoryMetrics() {
