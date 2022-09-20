2010e8f42a5c5e607fdf649a56bc74501d50f0b4
Dariusz Tomczyszyn <dariusz.tomczyszyn@swmansion.com>
2022-09-20 22:56:25 +0200

    feat(graph): graph init update

--- src/components/RepositoryPreview/RepositoryGraph/RepositoryGraph.tsx
+++ src/components/RepositoryPreview/RepositoryGraph/RepositoryGraph.tsx
@@ -1,36 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import ReactFlow, {
   addEdge,
   Controls,
   Background,
   useNodesState,
   useEdgesState,
+  Node,
 } from "react-flow-renderer";
 import dagre from "dagre";
 
-import { RepositoryResult } from "models/RepositoryResult";
-import { prepareNodes, placeNodesOnCanvas } from "utils/repositoryNodes";
-import Spinner from "components/Spinner";
+import { placeNodesOnCanvas, createNodesAndEdges } from "utils/repositoryNodes";
+import { BranchResult, CommitResult } from "models/RepositoryResult";
+import { AppDispatch } from "store/store";
+import { toggleCommitSelection } from "store/slices/commitsComparing";
+
+import CommitNode from "components/RepositoryPreview/RepositoryGraph/Node";
+
+interface RepositoryGraphProps {
+  branch: BranchResult;
+  select: boolean;
+  selectedCommits: CommitResult[];
+}
 
 const dagreGraph = new dagre.graphlib.Graph();
 dagreGraph.setDefaultEdgeLabel(() => ({}));
 
-type RepositoryGraphProps = {
-  status: "idle" | "loading";
-  error: string | null;
-  branches: RepositoryResult[];
-};
+const nodeTypes = { commit: CommitNode };
 
-const RepositoryGraph = ({ branches, status, error }: RepositoryGraphProps) => {
+const RepositoryGraph = ({
+  branch,
+  select,
+  selectedCommits,
+}: RepositoryGraphProps) => {
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
+  const dispatch: AppDispatch = useDispatch();
+
+  const updateNodes = useCallback(
+    (nodes: Node[], selectedCommits: CommitResult[]) => {
+      return nodes.map((node) => {
+        const isNodeSelected =
+          selectedCommits.findIndex(
+            (selectedCommit) => selectedCommit.sha === node.id
+          ) !== -1;
+        node.data = { ...node.data, selected: isNodeSelected };
+        return { ...node };
+      });
+    },
+    []
+  );
+
   useEffect(() => {
-    const { nodes: preparedNodes, edges: preparedEdges } =
-      prepareNodes(branches);
-    const { nodes: placedNodes, edges: placedEdges } = placeNodesOnCanvas(
-      preparedNodes,
-      preparedEdges,
+    const [initNodes, initEdges] = createNodesAndEdges(branch);
+    const [placedNodes, placedEdges] = placeNodesOnCanvas(
+      initNodes,
+      initEdges,
       dagreGraph,
       "TB",
       60,
@@ -38,14 +64,30 @@ const RepositoryGraph = ({ branches, status, error }: RepositoryGraphProps) => {
     );
     setNodes(placedNodes);
     setEdges(placedEdges);
-  }, [branches, setEdges, setNodes]);
+  }, [branch, setEdges, setNodes]);
+
+  useEffect(() => {
+    setNodes((prevNodes) => updateNodes(prevNodes, selectedCommits));
+  }, [selectedCommits, setNodes, updateNodes]);
 
   const onConnect = (params: any) => setEdges((eds) => addEdge(params, eds));
 
+  const onNodeClick = (event: any, node: Node) => {
+    console.log(node);
+    if (select) {
+      dispatch(toggleCommitSelection(node.data.commit));
+    } else {
+      // todo: fetch metrics
+    }
+  };
+
+  const openCommitDetails = (event: any, node: Node) => {};
+
+  const closeCommitDetails = (event: any, node: Node) => {};
+
   return (
-    <div className="whiteBox flex flex-1 md:flex-col md:h-full p-10">
-      {status === "loading" && <Spinner />}
-      {status === "idle" && branches.length > 0 && (
+    <>
+      {nodes.length && (
         <ReactFlow
           nodes={nodes}
           edges={edges}
@@ -55,12 +97,17 @@ const RepositoryGraph = ({ branches, status, error }: RepositoryGraphProps) => {
           attributionPosition="top-right"
           zoomOnScroll={false}
           panOnScroll={true}
+          onNodeClick={onNodeClick}
+          onNodeMouseEnter={openCommitDetails}
+          onNodeMouseLeave={closeCommitDetails}
+          defaultPosition={[nodes[0].position.x, nodes[0].position.y]}
+          nodeTypes={nodeTypes}
         >
           <Controls />
           <Background color="#aaa" gap={16} />
         </ReactFlow>
       )}
-    </div>
+    </>
   );
 };
 
